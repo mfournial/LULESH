f0121243c67c007ddab0543d7c7d754a13392495
Mayeul Fournial <mfournial@outlook.com>
2020-05-09 09:33:51 +0000

    Add musl compatibility

--- lulesh.h
+++ lulesh.h
@@ -19,6 +19,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <vector>
+#include <cstdint>
 
 //**************************************************
 // Allow flexibility for arithmetic representations 
